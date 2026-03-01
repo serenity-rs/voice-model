3ea2cb0029ab22e84acc863467a81718d51b6de6
beer-psi <92439990+beer-psi@users.noreply.github.com>
2026-03-01 20:40:08 +0700

    Fix binary data length check

--- src/binary.rs
+++ src/binary.rs
@@ -44,7 +44,7 @@ fn read_u16(data: &[u8]) -> Result<u16, BinaryError> {
 
 /// Deserialize a binary message from Discord (opcodes 25, 27, 29, 30)
 pub fn deserialize_binary_event(data: &[u8]) -> Result<crate::Event, BinaryError> {
-    if data.len() < 3 {
+    if data.len() < 1 {
         return Err(BinaryError::InsufficientData);
     }
 
